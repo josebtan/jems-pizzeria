f3f32064012a01357595c12eb735aae728af161b
JEMS App <jems@app.local>
2026-07-31 00:45:29 +0000

    Voltear horizontalmente la mascota en Ventas (misma posicion)

--- sw.js
+++ sw.js
@@ -4,13 +4,13 @@
 // como respaldo para que la app abra aunque no haya señal.
 // Los archivos externos (Firebase, Google Fonts) se dejan pasar directo a la red.
 
-const CACHE_NAME = "jems-shell-v8";
+const CACHE_NAME = "jems-shell-v9";
 
 const PRECACHE_URLS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./css/style.css?v=10",
+  "./css/style.css?v=11",
   "./js/app.js",
   "./js/data.js",
   "./js/firebase-config.js",
