b10bed18d8ca3aa56c73fb4074fb1abf33aa1c52
JEMS App <jems@app.local>
2026-07-31 00:32:40 +0000

    Quitar numeros (01/02/03) del menu lateral

--- sw.js
+++ sw.js
@@ -4,13 +4,13 @@
 // como respaldo para que la app abra aunque no haya señal.
 // Los archivos externos (Firebase, Google Fonts) se dejan pasar directo a la red.
 
-const CACHE_NAME = "jems-shell-v4";
+const CACHE_NAME = "jems-shell-v5";
 
 const PRECACHE_URLS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./css/style.css?v=6",
+  "./css/style.css?v=7",
   "./js/app.js",
   "./js/data.js",
   "./js/firebase-config.js",
